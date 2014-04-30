10cb56e33900a01ecdd4b82def56d369da188eec
Alex Meade <mr.alex.meade@gmail.com>
2014-04-30 14:08:50 -0400

    Forgot to add seven mode tests
    Change-Id: I7fb1371fb47340006a02ecd9afba0dfb31944f9f

---
+++ cinder/tests/volume/drivers/netapp/client/test_seven_mode.py
@@ -0,0 +1,150 @@
+# Copyright (c) 2014 NetApp, Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from lxml import etree
+import mock
+import uuid
+
+from cinder import test
+from cinder.volume.drivers.netapp import api as netapp_api
+from cinder.volume.drivers.netapp.client import seven_mode
+
+
+class NetApp7modeClientTestCase(test.TestCase):
+
+    def setUp(self):
+        super(NetApp7modeClientTestCase, self).setUp()
+        self.connection = mock.MagicMock()
+        self.fake_volume = str(uuid.uuid4())
+        self.client = seven_mode.Client(self.connection, [self.fake_volume])
+        self.fake_lun = str(uuid.uuid4())
+        self.fake_size = '1024'
+        self.fake_metadata = {
+            'OsType': 'linux',
+            'SpaceReserved': 'true',
+        }
+
+    def tearDown(self):
+        super(NetApp7modeClientTestCase, self).tearDown()
+
+    def test_get_target_details_no_targets(self):
+        response = netapp_api.NaElement(
+            etree.XML("""<results status="passed">
+                           <iscsi-portal-list-entries>
+                           </iscsi-portal-list-entries>
+                         </results>"""))
+        self.connection.invoke_successfully.return_value = response
+
+        target_list = self.client.get_target_details()
+
+        self.assertEqual([], target_list)
+
+    def test_get_target_details(self):
+        expected_target = {
+            "address": "127.0.0.1",
+            "port": "1337",
+            "tpgroup-tag": "7777",
+        }
+        response = netapp_api.NaElement(
+            etree.XML("""<results status="passed">
+                           <iscsi-portal-list-entries>
+                              <iscsi-portal-list-entry-info>
+                                <ip-address>%(address)s</ip-address>
+                                <ip-port>%(port)s</ip-port>
+                                <tpgroup-tag>%(tpgroup-tag)s</tpgroup-tag>
+                              </iscsi-portal-list-entry-info>
+                           </iscsi-portal-list-entries>
+                          </results>""" % expected_target))
+        self.connection.invoke_successfully.return_value = response
+
+        target_list = self.client.get_target_details()
+
+        self.assertEqual([expected_target], target_list)
+
+    def test_get_iscsi_service_details_with_no_iscsi_service(self):
+        response = netapp_api.NaElement(
+            etree.XML("""<results status="passed">
+                         </results>"""))
+        self.connection.invoke_successfully.return_value = response
+
+        iqn = self.client.get_iscsi_service_details()
+
+        self.assertEqual(None, iqn)
+
+    def test_get_iscsi_service_details(self):
+        expected_iqn = 'iqn.1998-01.org.openstack.iscsi:name1'
+        response = netapp_api.NaElement(
+            etree.XML("""<results status="passed">
+                            <node-name>%s</node-name>
+                         </results>""" % expected_iqn))
+        self.connection.invoke_successfully.return_value = response
+
+        iqn = self.client.get_iscsi_service_details()
+
+        self.assertEqual(expected_iqn, iqn)
+
+    def test_get_lun_list(self):
+        response = netapp_api.NaElement(
+            etree.XML("""<results status="passed">
+                           <luns>
+                            <lun-info></lun-info>
+                            <lun-info></lun-info>
+                           </luns>
+                          </results>"""))
+        self.connection.invoke_successfully.return_value = response
+
+        luns = self.client.get_lun_list()
+
+        self.assertEqual(2, len(luns))
+
+    def test_get_igroup_by_initiator_none_found(self):
+        initiator = 'initiator'
+        response = netapp_api.NaElement(
+            etree.XML("""<results status="passed">
+                           <initiator-groups>
+                           </initiator-groups>
+                         </results>"""))
+        self.connection.invoke_successfully.return_value = response
+
+        igroup = self.client.get_igroup_by_initiator(initiator)
+
+        self.assertEqual([], igroup)
+
+    def test_get_igroup_by_initiator(self):
+        initiator = 'initiator'
+        expected_igroup = {
+            "initiator-group-os-type": None,
+            "initiator-group-type": "1337",
+            "initiator-group-name": "vserver",
+        }
+        response = netapp_api.NaElement(
+            etree.XML("""<results status="passed">
+                           <initiator-groups>
+                             <initiator-group-info>
+                               <initiators>
+                                 <initiator-info>
+                                   <initiator-name>initiator</initiator-name>
+                                 </initiator-info>
+                               </initiators>
+    <initiator-group-type>%(initiator-group-type)s</initiator-group-type>
+    <initiator-group-name>%(initiator-group-name)s</initiator-group-name>
+                             </initiator-group-info>
+                           </initiator-groups>
+                         </results>""" % expected_igroup))
+        self.connection.invoke_successfully.return_value = response
+
+        igroup = self.client.get_igroup_by_initiator(initiator)
+
+        self.assertEqual([expected_igroup], igroup)
